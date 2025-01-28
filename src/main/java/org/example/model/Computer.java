b7c8506bf5ea958e185600825e9a5e47c0a14b36
maksJazzTeam <maksim.klyuev.jazzteam@gmail.com>
2025-01-28 12:35:14 +0300

    first commit

---
+++ src/main/java/org/example/model/Computer.java
@@ -0,0 +1,28 @@
+package org.example.model;
+
+public class Computer {
+    //Table 4->TR 4-> after TD "Операционная система"
+    private String os;
+    //Table 4->TR 15-> after TD "Тип цп"
+    private String cpu;
+    //Table 4->TR 16-> after TD "Системная плата"
+    private String motherboard;
+    //td "Тип памяти"
+    private String ramType;
+    //"Разъёмы ОЗУ"
+    private String slotsAmountRam;
+    //"Тип монитора"
+    private String display;
+    //вторая строка таблицы после таблицы "Видео Windows"
+    private String graphicCard;
+    //вторая строка таблицы после таблицы "Память"
+    private String ramVolume;
+
+
+
+
+    private String planksAmountRam;
+    private byte disksAmount;
+    private String disksType;
+    private String storageVolume;
+}
