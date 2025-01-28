b16f7b4cfe9a13d5b0aab6b49fdec86cd401e4ee
MaksKlyuev <klyuev.maksim.2000@gmail.com>
2025-01-28 19:00:18 +0300

    files names received

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.service.FileManipulationsService;
+
 public class App {
     public static void main(String[] args) {
-
+        FileManipulationsService obj = new FileManipulationsService();
+        //obj.getFilesNames();
+        for (String str: obj.getFilesNames()) {
+            System.out.println(str);
+        }
     }
 }
---
+++ src/main/java/org/example/service/FileManipulationsService.java
@@ -0,0 +1,21 @@
+package org.example.service;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManipulationsService {
+    public List<String> getFilesNames() {
+        ArrayList<String> filesNamesList = new ArrayList<>();
+        File folder = new File("src/main/resources/");
+        File[] listOfFiles = folder.listFiles();
+        if (listOfFiles != null) {
+            for (File listOfFile : listOfFiles) {
+                if (listOfFile.isFile()) {
+                    filesNamesList.add(listOfFile.getName());
+                }
+            }
+        }
+        return filesNamesList;
+    }
+}
